84da17ef3e6ad2cf31b25570769725cc832d0653
Gunner Wagoner <gunnerwagoner@gmail.com>
2022-01-23 18:43:09 -0600

    fixed all tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,10 @@
 const Manager = require("../lib/Manager");
 
 test("creates an manager object", () => {
-  const manager = new Manager("943");
+  const manager = new Manager("Gunner", "92", "gunnerwagoner@hotmail.com", "943");
 
+  expect(manager.name).toBe("Gunner");
+  expect(manager.id).toBe("92");
+  expect(manager.email).toBe("gunnerwagoner@hotmail.com");
   expect(manager.officeNumber).toBe("943");
 });
